eff1e8a65b880c37e0517caaf733c8e4310abdfd
Navaneet R Rao <navaneet517@gmail.com>
2024-09-13 20:11:55 +0530

    feat: express api app working with vercel

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { PrismaClient } = require("@prisma/client");
 
-const app = express();
-// const prisma = new PrismaClient();
+const app = express(); // express app
+const prisma = new PrismaClient();
 
 // Middleware
 app.use(cors());
@@ -28,13 +28,13 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 // API Routes
-const signupRoute = require("./src/api/sigup");
-const loginRoute = require("./src/api/login");
-const categoriesRoute = require("./src/api/categories");
-const postRoute = require("./src/api/post");
-const allpostRoute = require("./src/api/allposts");
-const commentRoute = require("./src/api/comments");
-const userRoutes = require("./src/api/userroutes");
+const signupRoute = require("./api/sigup");
+const loginRoute = require("./api/login");
+const categoriesRoute = require("./api/categories");
+const postRoute = require("./api/post");
+const allpostRoute = require("./api/allposts");
+const commentRoute = require("./api/comments");
+const userRoutes = require("./api/userroutes");
 
 app.use(signupRoute);
 app.use(loginRoute);
@@ -44,8 +44,11 @@ app.use(allpostRoute);
 app.use(commentRoute);
 app.use(userRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+module.exports = app;
